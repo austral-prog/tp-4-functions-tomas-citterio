5c43998eb0e88e464c93c37509a553f2b00f5344
tomas-citterio <tomcitterio08@gmail.com>
2026-04-13 19:13:26 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -5,13 +5,15 @@ def roots(a, b, c):
     discriminant = (b ** 2) - 4 * a * c
     if discriminant < 0:
         return "( )"
-    elif discriminant == 0:
-        r = -b / (2 * a)
-        return f"({r})"
-    else:
-        r1 = (-b + math.sqrt(discriminant)) / (2 * a)
-        r2 = (-b - math.sqrt(discriminant)) / (2 * a)
-        return f"({r1}, {r2})"
+    r1 = (-b + math.sqrt(discriminant)) / (2 * a)
+    r2 = (-b - math.sqrt(discriminant)) / (2 * a)
+    if r1 == r2:
+        return f"({r1})"
+    elif r1 == 0 and r2 == 0:
+        return "( )"
+        
+    return f"({r1},{r2})"    
+        
 
 
 def value_y(a, b, c, x):
