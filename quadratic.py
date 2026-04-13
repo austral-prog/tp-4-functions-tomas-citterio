7ae720b331edd8af5d9098622aecca3c752cf3e7
tomas-citterio <tomcitterio08@gmail.com>
2026-04-13 19:17:05 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,41 +1,22 @@
 import math
-
-
+ 
 def roots(a, b, c):
-    discriminant = (b ** 2) - 4 * a * c
-    if discriminant < 0:
+    discriminant = b**2 - 4*a*c
+    if discriminant > 0:
+        r1 = (-b + math.sqrt(discriminant)) / (2*a)
+        r2 = (-b - math.sqrt(discriminant)) / (2*a)
+        return f"({r1}, {r2})"
+    elif discriminant == 0:
+        r12 = -b / (2*a)
+        return f"({r12})"
+    else:
         return "( )"
-    r1 = (-b + math.sqrt(discriminant)) / (2 * a)
-    r2 = (-b - math.sqrt(discriminant)) / (2 * a)
-    if r1 == r2:
-        return f"({r1})"
-    elif r1 == 0 and r2 == 0:
-        return "( )"
-        
-    return f"({r1},{r2})"    
-        
-
-
+ 
 def value_y(a, b, c, x):
-    return a * x ** 2 + b * x + c
-
-
+    return a * x**2 + b * x + c
+ 
 def to_string(a, b, c):
-    if a == 0 and b == 0:
-        return f"f(x) ={c} "
-    if a ==0:
-        return f"f(x) = {b} * X + {c}"
-    if b == 0:
-        return f"f(x) = {a} * X^2 + {c}"
     return f"f(x) = {a} * X^2 + {b} * X + {c}"
-
-
+ 
 def derivation(a, b, c):
-    if a == 0 and b == 0:
-        return f"f'(x) = 0"
-    if a == 0:
-        return f"f'(x) = {b} "
-    if b == 0:
-        return f"f'(x) ={a * 2} * X"
-
-    return f"f'(x) = {a * 2} * X + {b}"
+    return f"f'(x) = {2*a}x + {b}"
