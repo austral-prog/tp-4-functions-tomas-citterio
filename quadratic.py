279507bd86444ccd85e3ab135adf62592ae7ad75
tomas-citterio <tomcitterio08@gmail.com>
2026-04-14 08:57:18 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -17,7 +17,7 @@ def value_y(a, b, c, x):
     return a * x ** 2 + b * x + c
 
 def to_string(a, b, c):
-    if a == 0 and b == 0:
+    if a==0 and b==0:
         return f"f(x) ={c} "
     if a == 0:
         return f"f(x) = {b} * X + {c}"
@@ -26,10 +26,10 @@ def to_string(a, b, c):
     return f"f(x) = {a} * X^2 + {b} * X + {c}"
 
 def derivation(a, b, c):
-    if a == 0 and b == 0:
+    if a==0 and b==0:
         return f"f'(x) = 0"
     if a == 0:
-        return f"f'(x) = {b} "
+        return f"f'(x) = {b}"
     if b == 0:
         return f"f'(x) ={a * 2} * X"
     return f"f'(x) = {a * 2} * X + {b}"
